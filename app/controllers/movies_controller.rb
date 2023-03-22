c2ae76850c4fba13968c34aef8b86043be82c75f
+ <+>
2023-03-22 18:03:55 +0300

    finished work

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,6 +10,31 @@ class MoviesController < ApplicationController
     movie = Movie.find(params[:id])
     render json: movie
   end
+ 
+  # def summary
+  #   "#{self.object.title} - #{self.object.description[0..49]}..."
+  # end
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+  
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
+  end
+ 
+  
+#   def show
+#   movie = Movie.find(params[:id])
+#   render json: movie.to_json(only: [:id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director])
+# end
+# def show
+#   post = Post.find(params[:id])
+#if we had a blogging app in which posts belong to authors
+#   render json: post.to_json(only: [:title, :description, :id], include: [author: { only: [:name]}])
+# end
+
 
   private
 
